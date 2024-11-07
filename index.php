64e4c2950c975fcf6ba1e59e0bdf406137c4da09
Axel Clody <axelclody@gmail.com>
2024-11-07 11:42:16 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+/*
+Theme Name: Nathalie Mota
+Theme URI: http://localhost/NathalieMota/
+Author: AC
+Author URI: http://localhost/NathalieMota/
+Description: Thème pour Nathalie Mota - Photographe Freelance
+Version: 1.0
+Tags: photography, motaphoto, photos
+Text Domain: nathalie mota
+*/
